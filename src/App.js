8bd36e7704b6de9387f36c62480acfddcd1fbc4e
cesarhj19 <cesarhj19@gmail.com>
2020-06-15 09:12:40 -0700

    test pass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Marked from 'marked';
 import './App.css';
 
 function App() {
+  const [content, setContent] = useState({editor: `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+    
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+  
+  There's also [links](https://www.freecodecamp.com), and
+  > Block Quotes!
+  
+  And if you want to get really crazy, even tables:
+  
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | ------------- 
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+  
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+  
+  
+  1. And there are numbererd lists too.
+  1. Use just 1s if you want! 
+  1. But the list goes on...
+  - Even if you use dashes or asterisks.
+  * And last but not least, let's not forget embedded images:
+  
+  ![React Logo w/ Text](https://goo.gl/Umyytc)
+  `});
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setContent({[e.target.name]: e.target.value})
+  }
+
+  function rawMarkup () {
+    let rawMarkup = Marked(content.editor, {sanitize: true});
+    return { __html: rawMarkup };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='top'>
+        <h1>Editor</h1>
+        <textarea name='editor' onChange={handleChange} id='editor' />
+      </div>
+      <div className='bottom'>
+        <h1>Previewer</h1>
+        <div id='preview' dangerouslySetInnerHTML={rawMarkup()}></div>
+      </div>
     </div>
   );
 }
